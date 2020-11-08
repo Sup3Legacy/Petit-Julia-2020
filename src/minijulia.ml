2821ca71094a6ec884d148a00e383153401ba478
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2020-11-08 18:36:10 +0100

    modification drapeau

--- src/minijulia.ml
+++ src/minijulia.ml
@@ -5,14 +5,14 @@ open Ast
 (* open Ast *)
 
 let file = ref "test.jl";;
-let affiche = true;;
+let notAffiche = ref false;;
 
 let handle () =
   let c = open_in !file in
   let lb = Lexing.from_channel c in
   let e = Parser.fichier Lexer.token lb  in
-  if affiche then print_endline (show_fichier e)
-  else print_endline !file (* On peut switch entre afficher le nom (ie. compil réussie) et afficher l'arbre généré *)
+  if !notAffiche then print_endline !file
+  else print_endline (show_fichier e) (* On peut switch entre afficher le nom (ie. compil réussie) et afficher l'arbre généré *)
 ;;
 
 
@@ -24,7 +24,9 @@ let set_filename n =
 
 let main () =
   begin
-    let speclist = [("-f", Arg.String(set_filename), "file to process.");] in
+    let speclist = [("-f", Arg.String(set_filename), "file to process.");
+    ("-disable_print", Arg.Set notAffiche, "remove the print of the abstract.");
+    ] in
     Arg.parse speclist (fun x -> ()) "Issouffle";
     handle ();
   end
