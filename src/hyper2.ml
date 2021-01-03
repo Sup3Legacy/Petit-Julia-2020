4b1c28f04061a9c68685ea626746f2443c710b3a
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-01-03 19:00:15 +0100

    fixed dimension > 1 array declaration + array function type

--- src/hyper2.ml
+++ src/hyper2.ml
@@ -1,5 +1,12 @@
 open Ast
 
+let arr = ref 0;;
+let newArr () =
+  let temp = !arr in
+  arr := !arr + 1;
+  temp
+;;
+
 let fusionPos p1 p2 =
   {ldeb = p1.ldeb; cdeb = p1.cdeb; lfin = p2.lfin; cfin = p2.cfin}
 
@@ -12,8 +19,9 @@ let rec build_array arr =
       | t :: q -> t, q
       | [] -> (posVide, Etrue), [])
   in
-  let creation = posVide, ElvalueAffect (posVide, Lident (posVide, "_temp_array"), (posVide, Eapplication(posVide, "newarray", [(posVide, Eentier (Int64.of_int l)); head]))) in (* Code pour créer l'array temporaire *)
-  (posVide, Ebloc1 (posVide, creation :: (List.mapi (fun indice x -> posVide, Eapplication (posVide, "_setelement", [(posVide, Elvalue (Lident (posVide, "_temp_array"))); (posVide, Eentier (Int64.of_int (indice))); x])) arr) @ [(posVide, Elvalue (Lident (posVide, "_temp_array")))] ))
+  let name = "_temp_array" ^ (string_of_int (newArr ())) in
+  let creation = posVide, ElvalueAffect (posVide, Lident (posVide, name), (posVide, Eapplication(posVide, "newarray", [(posVide, Eentier (Int64.of_int l)); head]))) in (* Code pour créer l'array temporaire *)
+  (posVide, Ebloc1 (posVide, creation :: (List.mapi (fun indice x -> posVide, Eapplication (posVide, "_setelement", [(posVide, Elvalue (Lident (posVide, name))); (posVide, Eentier (Int64.of_int (indice))); x])) arr) @ [(posVide, Elvalue (Lident (posVide, name)))] ))
 ;;
   
 
