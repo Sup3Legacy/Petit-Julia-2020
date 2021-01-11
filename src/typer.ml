6b22535f755c6baa49792b4bacb80968318bb94d
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-01-11 16:58:35 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/typer.ml
+++ src/typer.ml
@@ -55,7 +55,7 @@ let typeName (t:Astype.pjtype):string = match t with
 
 (* teste la correction d'une déclaration de structure et la rajoute aux différents environnements *)
 let parcoursStruct (sE:structEnv) (aE:argsEnv) (fE:funcEnv) (b,p,str,l):(structEnv * argsEnv * funcEnv) =
-  if str = "print" || str = "println" || str = "div" || str = "array_length" || str = "input_int" || str = "input_string" || str = "int_of_float" || str = "sqrt" then
+  if str = "print" || str = "println" || str = "div" || str = "array_length" || str = "input_int" || str = "input_string" || str = "int_of_float" || str = "sqrt" || str = "delay" || str = "timestamp" then
     error (str^" is not an allowed structuture name") p
   else
     if Tmap.mem str sE then error ("already defined structuture of name :"^str) p
@@ -82,7 +82,7 @@ let parcoursStruct (sE:structEnv) (aE:argsEnv) (fE:funcEnv) (b,p,str,l):(structE
 
 (* teste la correction d'une déclaration de fonction et la rajoute à l'environnement des fonctions *)
 let parcoursFonction (vE:varEnv) (fE:funcEnv) (sE:structEnv) (posStr, nameFunc, pL, posT, pjT, _):funcEnv =
-  if nameFunc = "print" || nameFunc = "println" || nameFunc = "div" || nameFunc = "array_length" || nameFunc = "input_int" || nameFunc = "input_string" || nameFunc = "int_of_float" || nameFunc = "sqrt"
+  if nameFunc = "print" || nameFunc = "println" || nameFunc = "div" || nameFunc = "array_length" || nameFunc = "input_int" || nameFunc = "input_string" || nameFunc = "int_of_float" || nameFunc = "sqrt" || nameFunc = "delay" || nameFunc = "timestamp"
   then error ("reserved name "^nameFunc) posStr
   else
     if Tmap.mem nameFunc vE
@@ -288,7 +288,7 @@ let rec testTypageE (isLoc:bool) (vE:varEnv) (fE:funcEnv) (sE:structEnv) (aE:arg
     end
   | Eapplication (pName, ident, eL) -> begin
       match ident with 
-      | "print" | "println" -> (Nothing, CallE ((ident, ISet.singleton 0), List.fold_right (fun (_, e) l -> testTypageE isLoc vE fE sE aE rT b e::l) eL []))
+      | "print" | "println" | "delay" -> (Nothing, CallE ((ident, ISet.singleton 0), List.fold_right (fun (_, e) l -> testTypageE isLoc vE fE sE aE rT b e::l) eL []))
       | "newarray" | "_getelement" | "_setelement" -> (Any, CallE ((ident, ISet.singleton 0), List.fold_right (fun (_, e) l -> testTypageE isLoc vE fE sE aE rT b e::l) eL []))
       | _ ->
       if Tmap.mem ident fE then
