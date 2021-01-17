7cd1106311781279d546474a7f398b486cd3dc03
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2021-01-17 16:23:44 +0100

    ajout compilRef.mli

---
+++ src/compilRef.mli
@@ -0,0 +1,16 @@
+
+val div : bool ref
+val char : bool ref
+val int : bool ref
+val float : bool ref
+
+val input_int : bool ref
+
+val ge : bool ref
+val g : bool ref
+val le : bool ref
+val l : bool ref
+
+val add : bool ref
+val mul : bool ref
+val sub : bool ref
